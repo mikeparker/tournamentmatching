1d689f0be59af5922674f64ab2e35b9d13452504
Mike Parker <mikkey@gmail.com>
2016-02-14 12:47:14 +0000

    Comment out the ladies just to try to get mens working in test

--- TournamentMatcher/TournamentMatcher.Tests/TournamentMatcherTests.cs
+++ TournamentMatcher/TournamentMatcher.Tests/TournamentMatcherTests.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace TournamentMatcher.Tests
@@ -17,7 +14,7 @@ namespace TournamentMatcher.Tests
 
             float lowestScore = 999999;
             List<SuggestedTournament> possibleTournaments = new List<SuggestedTournament>();
-            for (int i = 0; i < 1000; i++)
+            for (int i = 0; i < 1; i++)
             {
                 var y = new List<Player>();
 //                y.Add(new Player("Mike", ", -13f));
@@ -30,72 +27,72 @@ namespace TournamentMatcher.Tests
 //                y.Add(new Player("Jon Sharpe", ", -7f));
 //                y.Add(new Player("Matt Sinclair", ", -7f));
 
-y.Add(new Player("Judith	Ashman	", -3));
-y.Add(new Player("Sean	Blanchflower	", -3));
-y.Add(new Player("Naomi	Bowman	", -2));
-y.Add(new Player("Jack	Boyns	", -8));
-y.Add(new Player("Peter	Clapham	", -1));
-y.Add(new Player("Samuel	Crabb	", -1));
-y.Add(new Player("Martin	Crossley	", -11));
-y.Add(new Player("Zephanie	Curgenven	", -2));
-y.Add(new Player("Jack	Curtis	", -11));
-y.Add(new Player("Paul	Davies	", -2));
-y.Add(new Player("Julia	Frede	", -3));
-y.Add(new Player("Jenny	Garratt	", -2));
-y.Add(new Player("Hazel	Greetham	", -2));
-y.Add(new Player("David	Hamilton	", -3));
-y.Add(new Player("Martha	Hawker",	0));
-y.Add(new Player("Paul	Hilditch	", -5));
-y.Add(new Player("Michael	Ho	", -3));
-y.Add(new Player("Neil	Hunter	", -1));
-y.Add(new Player("Elliot	Jackson",	1));
-y.Add(new Player("Chris	Jenkins	", -2));
-y.Add(new Player("Hilary	Jones",	2));
-y.Add(new Player("Joseph	Jose	", -7));
-y.Add(new Player("Thomas	Keane	", -1));
-y.Add(new Player("Kian Sing	Low	", -4));
-y.Add(new Player("Charlie	Lowndes	", -3));
-y.Add(new Player("Deepa	Manthravadi",	3));
-y.Add(new Player("Kevin	McIntyre	", -1));
-y.Add(new Player("Douglas	McMillan	", -1));
-y.Add(new Player("Ivo	Miller	", -3));
-y.Add(new Player("Colin	Millerchip	", -2));
-y.Add(new Player("Vivek	Mishra	", -2));
-y.Add(new Player("Mark	Nelid	", -1));
-y.Add(new Player("Lorne	Noble",	1));
-y.Add(new Player("Chris	Parker	", -9));
-y.Add(new Player("Mike	Parker	", -13));
-y.Add(new Player("Simon	Parker	", -12));
-y.Add(new Player("Rodolph	Perfetta	", -3));
-y.Add(new Player("Bridget	Pickup",	2));
-y.Add(new Player("Charity	Pickup",	1));
-y.Add(new Player("Raith	Pickup	", -4));
-y.Add(new Player("David	Piggott	", -6));
-y.Add(new Player("Alexander	Ray	", -3));
-y.Add(new Player("Alison	Reed	", -3));
-y.Add(new Player("Shang-Yik	Reigh	", -4));
-y.Add(new Player("Lucy	Robertson	", -1));
-y.Add(new Player("Jonathan	Sharpe	", -7));
-y.Add(new Player("Samuel	Shepherd	", -8));
-y.Add(new Player("Matthew	Sinclair	", -7));
-y.Add(new Player("Carl	Skuce	", -7));
-y.Add(new Player("Steven	Slatter	", -2));
-y.Add(new Player("Dominique	Smith	", -1));
-y.Add(new Player("Lorna	Smith	", -2));
-y.Add(new Player("Richard	Smith	", -9));
-y.Add(new Player("Mark	Talbot	", -4));
-y.Add(new Player("Chris	Thomas	", -3));
-y.Add(new Player("Dan	Trowell	", -3));
-y.Add(new Player("Lynn	Trowell	", -3));
-y.Add(new Player("Andy	Turley	", -8));
-y.Add(new Player("Paul	Upton	", -1));
-y.Add(new Player("Ian	White	", -7));
-y.Add(new Player("Andy	Williamson	", -5));
-y.Add(new Player("Richard	Wilson	", -1));
-y.Add(new Player("Becca	Wisden	", -2));
-y.Add(new Player("Karl	Wycherley	", -2));
-y.Add(new Player("Kasia	Zalewska	", -1));
-y.Add(new Player("Sense	", -3));
+//y.Add(new Player("Judith	Ashman	", -3));
+y.Add(new Player("Sean Blanchflower", -3));
+//y.Add(new Player("Naomi Bowman", -2));
+y.Add(new Player("Jack Boyns", -8));
+y.Add(new Player("Peter Clapham", -1));
+y.Add(new Player("Samuel Crabb", -1));
+y.Add(new Player("Martin Crossley", -11));
+//y.Add(new Player("Zephanie Curgenven", -2));
+y.Add(new Player("Jack Curtis", -11));
+y.Add(new Player("Paul Davies", -2));
+//y.Add(new Player("Julia Frede", -3));
+//y.Add(new Player("Jenny Garratt", -2));
+//y.Add(new Player("Hazel Greetham", -2));
+y.Add(new Player("David Hamilton", -3));
+//y.Add(new Player("Martha Hawker", 0));
+y.Add(new Player("Paul Hilditch", -5));
+y.Add(new Player("Michael Ho", -3));
+y.Add(new Player("Neil Hunter", -1));
+y.Add(new Player("Elliot Jackson", 1));
+y.Add(new Player("Chris Jenkins", -2));
+//y.Add(new Player("Hilary Jones", 2));
+y.Add(new Player("Joseph Jose", -7));
+y.Add(new Player("Thomas Keane", -1));
+y.Add(new Player("Kian Sing Low", -4));
+y.Add(new Player("Charlie Lowndes", -3));
+//y.Add(new Player("Deepa Manthravadi", 3));
+y.Add(new Player("Kevin McIntyre", -1));
+y.Add(new Player("Douglas McMillan", -1));
+y.Add(new Player("Ivo Miller", -3));
+y.Add(new Player("Colin Millerchip", -2));
+y.Add(new Player("Vivek Mishra", -2));
+y.Add(new Player("Mark Nelid", -1));
+y.Add(new Player("Lorne Noble", 1));
+y.Add(new Player("Chris Parker", -9));
+y.Add(new Player("Mike Parker", -13));
+y.Add(new Player("Simon Parker", -12));
+y.Add(new Player("Rodolph Perfetta", -3));
+//y.Add(new Player("Bridget Pickup", 2));
+//y.Add(new Player("Charity Pickup", 1));
+y.Add(new Player("Raith Pickup", -4));
+y.Add(new Player("David Piggott", -6));
+y.Add(new Player("Alexander Ray", -3));
+//y.Add(new Player("Alison Reed", -3));
+y.Add(new Player("Shang-Yik Reigh", -4));
+//y.Add(new Player("Lucy Robertson", -1));
+y.Add(new Player("Jonathan Sharpe", -7));
+y.Add(new Player("Samuel Shepherd", -8));
+y.Add(new Player("Matthew Sinclair", -7));
+y.Add(new Player("Carl Skuce", -7));
+y.Add(new Player("Steven Slatter", -2));
+//y.Add(new Player("Dominique Smith", -1));
+//y.Add(new Player("Lorna Smith", -2));
+y.Add(new Player("Richard Smith", -9));
+y.Add(new Player("Mark Talbot", -4));
+y.Add(new Player("Chris Thomas", -3));
+y.Add(new Player("Dan Trowell", -3));
+//y.Add(new Player("Lynn Trowell", -3));
+y.Add(new Player("Andy Turley", -8));
+y.Add(new Player("Paul Upton", -1));
+y.Add(new Player("Ian White", -7));
+y.Add(new Player("Andy Williamson", -5));
+y.Add(new Player("Richard Wilson", -1));
+//y.Add(new Player("Becca Wisden", -2));
+y.Add(new Player("Karl Wycherley", -2));
+//y.Add(new Player("Kasia Zalewska", -1));
+y.Add(new Player("Sense", -3));
                 var sut = new TournamentMatcher(y);
 
                 var result = sut.CreateRandomisedTournament(6);
@@ -112,6 +109,8 @@ y.Add(new Player("Sense	", -3));
             }
 
             var orderedTournaments = possibleTournaments.OrderBy(t => t.Score);
+
+            orderedTournaments.First().PrintToDebug();
         }
 
         [Test]
