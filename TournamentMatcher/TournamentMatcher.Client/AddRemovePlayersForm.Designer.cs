ce65226ec8f01c76256117706247546e56fcb22d
Mike Parker <mikkey@gmail.com>
2016-04-02 18:36:42 +0100

    Add / remove players buttons working

--- TournamentMatcher/TournamentMatcher.Client/AddRemovePlayersForm.Designer.cs
+++ TournamentMatcher/TournamentMatcher.Client/AddRemovePlayersForm.Designer.cs
@@ -33,47 +33,65 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AllowUserToResizeRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 25);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView1.Size = new System.Drawing.Size(306, 349);
             this.dataGridView1.TabIndex = 0;
             // 
             // dataGridView2
             // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.AllowUserToResizeRows = false;
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(405, 12);
+            this.dataGridView2.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView2.Location = new System.Drawing.Point(405, 25);
             this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView2.Size = new System.Drawing.Size(326, 349);
             this.dataGridView2.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(324, 119);
+            this.button1.Location = new System.Drawing.Point(324, 132);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 34);
             this.button1.TabIndex = 2;
             this.button1.Text = "Add >>";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(324, 159);
+            this.button2.Location = new System.Drawing.Point(324, 172);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 34);
             this.button2.TabIndex = 3;
             this.button2.Text = "<< Remove";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(656, 367);
+            this.button3.Location = new System.Drawing.Point(656, 380);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 34);
             this.button3.TabIndex = 4;
@@ -81,11 +99,31 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Available players";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(402, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Players in tournament";
+            // 
             // AddRemovePlayersForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(743, 413);
+            this.ClientSize = new System.Drawing.Size(743, 425);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -96,6 +134,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -106,5 +145,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- TournamentMatcher/TournamentMatcher.Client/AddRemovePlayersForm.cs
+++ TournamentMatcher/TournamentMatcher.Client/AddRemovePlayersForm.cs
@@ -14,6 +14,8 @@ namespace TournamentMatcher.Client
     public partial class AddRemovePlayersForm : Form
     {
         private List<Player> allPossiblePlayers;
+        private BindingList<Player> currentPlayersNotPlaying;
+        private BindingList<Player> currentTournamentPlayers;
 
         public AddRemovePlayersForm()
         {
@@ -22,12 +24,66 @@ namespace TournamentMatcher.Client
 
         public void SetPlayers(List<Player> allPossiblePlayers)
         {
+            this.currentPlayersNotPlaying = new BindingList<Player>(allPossiblePlayers);
             this.allPossiblePlayers = allPossiblePlayers;
+            currentTournamentPlayers = new BindingList<Player>();
+            SetDataGrid(this.dataGridView1, this.currentPlayersNotPlaying);
+            SetDataGrid(this.dataGridView2, this.currentTournamentPlayers);
+        }
+
+        private void SetDataGrid(DataGridView dataGridView, BindingList<Player> playersList)
+        {
+            dataGridView.DataSource = playersList;
+            dataGridView.Columns[2].Visible = false;
+            dataGridView.Columns[3].Visible = false;
+            dataGridView.Columns[4].Visible = false;
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        // Add players
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MovePlayers(this.dataGridView1, this.currentPlayersNotPlaying, this.currentTournamentPlayers);
+        }
+
+        private void MovePlayers(DataGridView dataGridView, BindingList<Player> playersToMoveFrom, BindingList<Player> playersToMoveTo)
+        {
+            Int32 selectedRowCount = dataGridView.Rows.GetRowCount(DataGridViewElementStates.Selected);
+            if (selectedRowCount <= 0)
+            {
+                return;
+            }
+
+            var indexesToMove = new HashSet<int>();
+            for (int i = 0; i < selectedRowCount; i++)
+            {
+                var index = dataGridView.SelectedRows[i].Index;
+                indexesToMove.Add(index);
+            }
+
+            MoveIndexesBetweenLists(indexesToMove, playersToMoveFrom, playersToMoveTo);
+        }
+
+        private void MoveIndexesBetweenLists(HashSet<int> indexesToMove, BindingList<Player> playersToMoveFrom, BindingList<Player> playersToMoveTo)
+        {
+            var firstListLength = playersToMoveFrom.Count;
+            for (int i = firstListLength; i >= 0; i--)
+            {
+                if (indexesToMove.Contains(i))
+                {
+                    playersToMoveTo.Add(playersToMoveFrom[i]);
+                    playersToMoveFrom.RemoveAt(i);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MovePlayers(this.dataGridView2, this.currentTournamentPlayers, this.currentPlayersNotPlaying);
         }
     }
 }
